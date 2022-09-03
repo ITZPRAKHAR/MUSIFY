44190627263f8ace2aa535b3eed13b24dffaeaee
Prakhar Singh <100228274+ITZPRAKHAR@users.noreply.github.com>
2022-09-04 03:23:28 +0530

    Added Songs and their play-pause functionality

--- MUSIFY/js/musify.js
+++ MUSIFY/js/musify.js
@@ -1,2 +1,58 @@
-//under progress
-let bar = document.getElementById("ProgressBar")
+// under progress
+
+let song1 = new Audio("music/1.mp3");
+// song.play();
+let song2 = new Audio("music/2.mp3");
+let song3 = new Audio("music/3.mp3");
+let song4 = new Audio("music/4.mp3");
+
+
+
+function one() {
+    song1.play();
+
+}
+
+function two() {
+    song2.play();
+}
+
+function three() {
+    song3.play();
+}
+
+function four() {
+    song4.play();
+}
+
+function lol(){
+    song1.pause();
+}
+
+// let b = document.getElementsByClassName("master");
+// let c = 0;
+
+// function playpuase(){
+//        if(c==0){
+//         c=1;
+//         song1.play();
+//        }
+//        else {
+//         c=0;
+//         song1.pause();
+//        }
+// }
+
+function pause1(){
+    song1.pause();
+}
+function pause2(){
+    song2.pause();
+}
+function pause3(){
+    song3.pause();
+}
+function pause4(){
+    song4.pause();
+}
+
